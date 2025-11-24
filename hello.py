e51da010a10b83fa3c778960114813a1a509f145
riley688 <s3546536@students.fscj.edu>
2025-11-23 21:39:19 -0500

    Update hello.py
    Updated code to say "Welcome to GitHub!"

--- hello.py
+++ hello.py
@@ -1,5 +1,11 @@
-#Firstname Lastname 
-# 11/15/2024
+# Riley Williams
+# 11/15/2025
 # A simple Python program to print a mesagge
 
+# Riley Williams
+# 11/23/2025
+# added new message 
+
 print("Hello, World!")
+
+print("Welcome to GitHub!")
